3f0c718df1a4eadd0a239ac7e67e1b124466e1f2
weiteck <chris@chrisprice.au>
2026-05-31 14:18:49 +1000

    restore credits in 'about' window

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -23,12 +23,18 @@ impl SimpleComponent for AboutModel {
     adw::AboutWindow {
       set_application_name: APP_NAME_PRETTY,
       set_application_icon: "lyricade",
+      set_copyright: "Copyright © 2026 Chris Price",
       set_developer_name: "Chris Price",
       set_developers: &["Chris Price <fair.lake5766@fastmail.com>"],
       set_license_type: gtk::License::Apache20,
       set_license: include_str!("../../LICENSE"),
       set_website: &format!("https://github.com/weiteck/{APP_NAME}"),
+      set_comments: r"Thanks go to <b>tranxuanthang</b> and the <b>lrclib.net</b> contributors for creating the service that this app relies upon.
+
+    <i>github.com/tranxuanthang/lrclib</i>",
       set_version: env!("CARGO_PKG_VERSION"),
+      set_release_notes_version: env!("CARGO_PKG_VERSION"),
+      set_release_notes: "<p>Initial release</p>",
     }
   }
 
